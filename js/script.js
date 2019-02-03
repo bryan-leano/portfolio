e410c45a7602192e1ede4bd6b458ea1d813730af
Bryan Leanoe <bryan.leano@gmail.com>
2019-02-02 21:28:29 -0700

    add pie chart options

--- js/script.js
+++ js/script.js
@@ -15,24 +15,34 @@ $(document).ready(function() {
 	});
 
 	$('.owl-carousel').owlCarousel({
-	    loop:true,
-	    margin:10,
-	    nav:true,
+      loop:true,
+      items: 4,
 	    responsive:{
 	        0:{
-	            items:1
+	          items:1
 	        },
-	        600:{
-	            items:3
+	        480:{
+	          items:2
 	        },
-	        1000:{
-	            items:5
-	        }
+	        768:{
+	          items:3
+          },
+          938:{
+            items:4
+        }
 	    }
   });
   
   $('.chart').easyPieChart({
-    //your options goes here
+    easing: 'easeInOut',
+    barColor: '#fff',
+    trackColor: false,
+    scaleColor: false,
+    lineWidth: 4,
+    size: 152,
+    onStep: function(from, to, percent) {
+      $(this.el).find('.percent').text(Math.round(percent));
+    }
   });
 
 });
